5c79c46a73ce18913b3d65cf339c4ee75444d6c2
hyj1991 <yeekwanvong@gmail.com>
2021-09-23 07:18:44 +0800

    fix: endless loop at oom hook

--- src/hooks/out_of_memory.cc
+++ src/hooks/out_of_memory.cc
@@ -14,14 +14,24 @@ static const char module_type[] = "out_of_memory";
 
 static bool oom_flag = false;
 
+size_t NearHeapLimitCallback(void* raw_data, size_t current_heap_limit,
+                             size_t initial_heap_limit) {
+  Info(module_type, "current_heap_limit is %d.", current_heap_limit);
+  return initial_heap_limit + 200 * 1024 * 1024;
+}
+
 static void OnOutOfMemoryError(const char* location, bool is_heap_oom) {
+  // avoid endless loop
   if (oom_flag) {
     Info(module_type, "heapdump hook before oom has been executed.");
     return;
   }
-
   oom_flag = true;
 
+  Isolate* isolate = Isolate::GetCurrent();
+  isolate->AddNearHeapLimitCallback(NearHeapLimitCallback, nullptr);
+
+  // dump snapshot
   string filepath = GetLogDir() + GetSep() + "x-oom-" + to_string(GetPid()) +
                     "-" + ConvertTime("%Y%m%d") + "-" + RandNum() +
                     ".heapsnapshot";
