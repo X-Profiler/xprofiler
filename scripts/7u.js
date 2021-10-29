47f3635417e6639a3decf74498bdb8ef3e34cc03
hyj1991 <yeekwanvong@gmail.com>
2021-10-29 18:06:22 +0800

    feat: support node-v17.x

--- scripts/7u.js
+++ scripts/7u.js
@@ -3,11 +3,12 @@
 const build = require('./build');
 
 const nodeVersions = [
-  'node-v12.18.3',
+  'node-v12.22.7',
   'node-v13.14.0',
-  'node-v14.6.0',
-  'node-v15.1.0',
-  'node-v16.1.0',
+  'node-v14.18.1',
+  'node-v15.14.0',
+  'node-v16.13.0',
+  'node-v17.0.1',
 ];
 
 build(nodeVersions);
