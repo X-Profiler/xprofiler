e6c60e64489ab2bb50f786cd6fb28464f7825399
hyj1991 <yeekwanvong@gmail.com>
2021-11-01 10:16:12 +0800

    feat: support node-v17.x
    PR-URL: https://github.com/X-Profiler/xprofiler/pull/104#issuecomment-954623194
    Reviewed-BY: hyj1991 <yeekwanvong@gmail.com>

--- scripts/6u.js
+++ scripts/6u.js
@@ -5,7 +5,7 @@ const build = require('./build');
 const nodeVersions = [
   'node-v8.17.0',
   'node-v9.11.2',
-  'node-v10.22.0',
+  'node-v10.24.1',
   'node-v11.15.0',
 ];
 
--- scripts/common.js
+++ scripts/common.js
@@ -7,11 +7,12 @@ const nodeVersions = [
   'node-v9.11.2',
   'node-v10.24.1',
   'node-v11.15.0',
-  'node-v12.19.1',
+  'node-v12.22.7',
   'node-v13.14.0',
-  'node-v14.16.1',
+  'node-v14.18.1',
   'node-v15.14.0',
-  'node-v16.1.0',
+  'node-v16.13.0',
+  'node-v17.0.1',
 ];
 
 build(nodeVersions);
