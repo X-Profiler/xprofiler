1e3ddb77e5bc9966619b72aef0f3da21b80b4b36
hyj1991 <yeekwanvong@gmail.com>
2021-12-14 07:42:05 +0800

    feat: new cpuprofile format (#113)
    PR-URL: https://github.com/X-Profiler/xprofiler/pull/113
    Reviewed-BY: hyj1991 <yeekwanvong@gmail.com>

--- src/commands/cpuprofiler/cpu_profile_node.cc
+++ src/commands/cpuprofiler/cpu_profile_node.cc
@@ -10,25 +10,32 @@ void ProfileNode::SerializeNode(const CpuProfileNode *node,
   Utf8String funcion_name(node->GetFunctionName());
   Utf8String url(node->GetScriptResourceName());
 
-  // set parent
+  // set node
+  writer->json_start();
+  writer->json_keyvalue("id", node->GetNodeId());
+  writer->json_keyvalue("hitCount", node->GetHitCount());
+  // set call frame
+  writer->json_objectstart("callFrame");
   writer->json_keyvalue("functionName", *funcion_name);
+  writer->json_keyvalue("scriptId", node->GetScriptId());
+  writer->json_keyvalue("bailoutReason", node->GetBailoutReason());
   writer->json_keyvalue("url", *url);
   writer->json_keyvalue("lineNumber", node->GetLineNumber());
   writer->json_keyvalue("columnNumber", node->GetColumnNumber());
-  writer->json_keyvalue("bailoutReason", node->GetBailoutReason());
-  writer->json_keyvalue("id", node->GetNodeId());
-  writer->json_keyvalue("scriptId", node->GetScriptId());
-  writer->json_keyvalue("hitCount", node->GetHitCount());
+  writer->json_objectend();
 
-  // set children recursively
+  // set children
   int32_t count = node->GetChildrenCount();
   writer->json_arraystart("children");
   for (int32_t index = 0; index < count; index++) {
-    writer->json_start();
-    ProfileNode::SerializeNode(node->GetChild(index), writer);
-    writer->json_end();
+    writer->json_element(node->GetChild(index)->GetNodeId());
   }
   writer->json_arrayend();
+  writer->json_end();
+
+  for (int32_t index = 0; index < count; index++) {
+    ProfileNode::SerializeNode(node->GetChild(index), writer);
+  }
 }
 
 }  // namespace xprofiler
