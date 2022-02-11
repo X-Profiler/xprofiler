b4907c823e0c25684a1f31ad7add0a531ee9d6df
legendecas <legendecas@gmail.com>
2022-02-11 14:53:57 +0800

    feat: per-isolate cpu profiler

--- src/commands/cpuprofiler/cpu_profile.h
+++ src/commands/cpuprofiler/cpu_profile.h
@@ -1,17 +1,19 @@
-#ifndef _SRC_COMMANDS_CPUPROFILER_CPU_PROFILE_H
-#define _SRC_COMMANDS_CPUPROFILER_CPU_PROFILE_H
+#ifndef XPROFILER_SRC_COMMANDS_CPUPROFILER_CPU_PROFILE_H
+#define XPROFILER_SRC_COMMANDS_CPUPROFILER_CPU_PROFILE_H
 
 #include "nan.h"
+#include "util.h"
 #include "v8-profiler.h"
 
 namespace xprofiler {
-using std::string;
-using v8::CpuProfile;
+class CpuProfile {
+  static void DeleteCpuProfile(const v8::CpuProfile* profile);
 
-class Profile {
  public:
-  static void Serialize(const CpuProfile* node, string filename);
+  using CpuProfilePtr = DeleteFnPtr<const v8::CpuProfile, DeleteCpuProfile>;
+  static void Serialize(v8::Isolate* isolate, CpuProfilePtr node,
+                        std::string filename);
 };
 
 }  // namespace xprofiler
-#endif
+#endif /* XPROFILER_SRC_COMMANDS_CPUPROFILER_CPU_PROFILE_H */
--- src/commands/cpuprofiler/cpu_profiler.h
+++ src/commands/cpuprofiler/cpu_profiler.h
@@ -1,20 +1,37 @@
-#ifndef _SRC_COMMANDS_CPUPROFILER_CPU_PROFILER_H
-#define _SRC_COMMANDS_CPUPROFILER_CPU_PROFILER_H
+#ifndef XPROFILER_SRC_COMMANDS_CPUPROFILER_CPU_PROFILER_H
+#define XPROFILER_SRC_COMMANDS_CPUPROFILER_CPU_PROFILER_H
 
 #include "node.h"
+#include "util.h"
 #include "v8-profiler.h"
 
 namespace xprofiler {
-using std::string;
 
-class Profiler {
+class CpuProfiler final {
+  static void DeleteCpuProfiler(v8::CpuProfiler* profiler);
+
+  using CpuProfilerPtr = DeleteFnPtr<v8::CpuProfiler, DeleteCpuProfiler>;
+
  public:
-  Profiler();
-  virtual ~Profiler();
-  static void StartProfiling(string title);
-  static void StopProfiling(string title, string filename);
-  static void SetSamplingInterval(uint32_t sample);
+  static void StartProfiling(v8::Isolate* isolate, std::string title);
+  static void StopProfiling(v8::Isolate* isolate, std::string title,
+                            std::string filename);
+  ~CpuProfiler();
+
+  int started_profiles_count() { return started_profiles_count_; }
+
+ private:
+  CpuProfiler(v8::Isolate* isolate);
+
+  void StartProfiling(std::string title);
+  void StopProfiling(std::string title, std::string filename);
+
+  void SetSamplingInterval(uint32_t sample);
+
+  v8::Isolate* isolate_;
+  int started_profiles_count_ = 0;
+  CpuProfilerPtr cpu_profiler_ = nullptr;
 };
 }  // namespace xprofiler
 
-#endif
+#endif /* XPROFILER_SRC_COMMANDS_CPUPROFILER_CPU_PROFILER_H */
