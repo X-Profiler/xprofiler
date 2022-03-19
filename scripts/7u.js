57fe96e15894d39fe253b14bf414c052c770846f
hyj1991 <yeekwanvong@gmail.com>
2022-03-19 19:53:45 +0800

    Release 1.4.0

--- scripts/7u.js
+++ scripts/7u.js
@@ -3,12 +3,12 @@
 const build = require('./build');
 
 const nodeVersions = [
-  'node-v12.22.7',
+  'node-v12.22.11',
   'node-v13.14.0',
-  'node-v14.18.2',
+  'node-v14.19.1',
   'node-v15.14.0',
-  'node-v16.13.1',
-  'node-v17.2.0',
+  'node-v16.14.2',
+  'node-v17.7.2',
 ];
 
 build(nodeVersions);
