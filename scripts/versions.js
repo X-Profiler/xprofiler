27f95d037f246da80cde5026d7cf2f7bf7b89349
hyj1991 <yeekwanvong@gmail.com>
2022-10-05 10:19:43 +0800

    Release 2.2.0
    Commits:
    
      - [1538d8c] fix: output native trace if HAVE_EXECINFO_H defined
      - [5c594a0] fix: remove upload
      - [b4feaf4] chore: update deps
      - [29d8b72] fix: fatalerror hook not working in node-v18.8.0
      - [92f651c] fix: fix nested gc callback
    
    PR-URL: https://github.com/X-Profiler/xprofiler/pull/197

--- scripts/versions.js
+++ scripts/versions.js
@@ -3,12 +3,12 @@
 exports.os7u = [
   'node-v12.22.12',
   'node-v13.14.0',
-  'node-v14.19.3',
+  'node-v14.20.1',
   'node-v15.14.0',
-  'node-v16.15.1',
+  'node-v16.17.1',
   'node-v17.9.1',
 ];
 
 exports.os8u = [
-  'node-v18.4.0',
+  'node-v18.10.0',
 ];
