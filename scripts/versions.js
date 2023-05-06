f2cd2c39708200c087ab4f2bace6c856adf7a88b
易容 (hyj1991) <yeekwanvong@gmail.com>
2023-05-06 17:36:21 +0800

    Release 2.5.2
    Commits:
    
      - [47d814f] feat: support node-v20.x
    
    PR-URL: https://github.com/X-Profiler/xprofiler/pull/226

--- scripts/versions.js
+++ scripts/versions.js
@@ -1,16 +1,22 @@
 'use strict';
 
+const os = require('os');
+
 exports.os7u = [
   'node-v12.22.12',
   'node-v13.14.0',
-  'node-v14.21.2',
+  'node-v14.21.3',
   'node-v15.14.0',
-  'node-v16.19.0',
+  'node-v16.20.0',
   'node-v17.9.1',
 ];
 
+if (os.platform() === 'darwin' && os.arch() === 'arm64') {
+  exports.os7u = exports.os7u.slice(4);
+}
+
 exports.os8u = [
-  'node-v18.13.0',
-  'node-v19.4.0',
-  'node-v20.0.0',
+  'node-v18.16.0',
+  'node-v19.9.0',
+  'node-v20.1.0',
 ];
