cfd18dda3b80e1bb4791370c365cd56c51f0986f
hyj1991 <yeekwanvong@gmail.com>
2023-09-05 16:03:19 +0800

    Release v2.5.3

--- scripts/versions.js
+++ scripts/versions.js
@@ -7,7 +7,7 @@ exports.os7u = [
   'node-v13.14.0',
   'node-v14.21.3',
   'node-v15.14.0',
-  'node-v16.20.0',
+  'node-v16.20.2',
   'node-v17.9.1',
 ];
 
@@ -16,7 +16,7 @@ if (os.platform() === 'darwin' && os.arch() === 'arm64') {
 }
 
 exports.os8u = [
-  'node-v18.16.0',
+  'node-v18.17.1',
   'node-v19.9.0',
-  'node-v20.1.0',
+  'node-v20.6.0',
 ];
