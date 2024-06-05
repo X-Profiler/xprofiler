0f4e5a83d1d3e0f15c541f7de1050b78d76e2672
killa <killa07071201@gmail.com>
2024-06-05 09:24:02 +0800

    feat: support XPROFILER_PREFIX env to set .xprofile dir (#244)

---
+++ test/utils.test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const utils = require('../lib/utils');
+const assert = require('assert');
+const mm = require('mm');
+const os = require('os');
+
+describe('utils.test.js', function () {
+  afterEach(mm.restore);
+
+  describe('get prefix dir', () => {
+    it('should work with XPROFILER_PREFIX', () => {
+      mm(process.env, 'XPROFILER_PREFIX', '/tmp');
+      const p = utils.getXprofilerPath();
+      assert.equal(p, '/tmp/.xprofiler');
+    });
+
+    it('should work default is home', () => {
+      mm(os, 'homedir', () => {
+        return '/home/xxx'
+      });
+      const p = utils.getXprofilerPath();
+      assert.equal(p, '/home/xxx/.xprofiler');
+    });
+  });
+});
